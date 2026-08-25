4a770bc0f9740053c54b42caea69a6f0008e8c67
LY-MENGKORNG <mengkorng.ly@student.passerellesnumeriques.org>
2026-08-25 16:06:03 +0700

    feat(structure-flutter-app): rename app flutter to client

---
+++ client/.widget_preview/lib/src/dtd/dtd_connection_info.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: implementation_imports
+
+const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:49307/j2nl3igNti4=';
+const String kWidgetPreviewService =
+    'widget-preview-259e778f-e6b3-46a3-96c4-9f2cbccc5d62';
+const String kWidgetPreviewScaffoldStream =
+    'WidgetPreviewScaffold-259e778f-e6b3-46a3-96c4-9f2cbccc5d62';
+const String kProjectRootPath =
+    r'/Users/mengkorngly/personal/learning/dart/it-support-app/app';
