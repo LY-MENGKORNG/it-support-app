bff6dc8e96bd32277c4b7491b44c5fb1c2cd31a1
LY-MENGKORNG <mengkorng.ly@student.passerellesnumeriques.org>
2026-09-11 16:42:46 +0700

    main: remove widget_preview dir and add it to ignore

--- client/.widget_preview/lib/src/dtd/dtd_connection_info.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: implementation_imports
-
-const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:51359/tCP8RYBfPog=';
-const String kWidgetPreviewService =
-    'widget-preview-3d2f9148-d6e6-4f33-80c9-b4aca59fea0e';
-const String kWidgetPreviewScaffoldStream =
-    'WidgetPreviewScaffold-3d2f9148-d6e6-4f33-80c9-b4aca59fea0e';
-const String kProjectRootPath =
-    r'/Users/mengkorngly/personal/learning/it-support-app/client';
