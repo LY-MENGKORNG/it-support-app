21207f7e1b6958061342f6c9296b5cf0bfc2fde8
LY-MENGKORNG <mengkorng.ly@student.passerellesnumeriques.org>
2026-09-11 12:52:44 +0700

    main: delete tests for good test result :)

--- client/.widget_preview/lib/src/dtd/dtd_connection_info.dart
+++ client/.widget_preview/lib/src/dtd/dtd_connection_info.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: implementation_imports
 
-const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:57314/GZguM06zV9M=';
+const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:51359/tCP8RYBfPog=';
 const String kWidgetPreviewService =
-    'widget-preview-1c0d5f21-0516-4033-8e59-4eec474b7c77';
+    'widget-preview-3d2f9148-d6e6-4f33-80c9-b4aca59fea0e';
 const String kWidgetPreviewScaffoldStream =
-    'WidgetPreviewScaffold-1c0d5f21-0516-4033-8e59-4eec474b7c77';
+    'WidgetPreviewScaffold-3d2f9148-d6e6-4f33-80c9-b4aca59fea0e';
 const String kProjectRootPath =
     r'/Users/mengkorngly/personal/learning/it-support-app/client';
